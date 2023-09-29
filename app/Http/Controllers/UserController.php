4008dae27b6c5796e35e575d69484a3aa35aba6e
Ramon-Rogerio <chanim1810@gmail.com>
2023-09-29 00:01:09 -0300

    adição de metodos no user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,39 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('admin.users.listing');
+        return view('admin.home');
     }
+
+    public function create()
+    {
+        // Exibir o formulário de criação
+    }
+
+    public function store(Request $request)
+    {
+        // Salvar um novo recurso no banco de dados
+    }
+
+    public function show($id)
+    {
+        // Exibir um recurso específico
+    }
+
+    public function edit($id)
+    {
+        // Exibir o formulário de edição
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Atualizar um recurso no banco de dados
+    }
+
+    public function destroy($id)
+    {
+        // Excluir um recurso do banco de dados
+    }
+ 
 }
